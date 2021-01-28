0525a57d55dd8e7ab863c6016f19c50e49271855
anngri-com <anna.gritsay@gmail.com>
2021-01-28 11:17:40 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const get = element => document.getElementById(element);
+
+let open = get("menu-btn");
+let nav = get("nav");
+let exit = get("exit-btn");
+let wrapper = get("container");
+
+open.addEventListener('click', () => {
+    nav.classList.add('open-nav');
+    wrapper.style.filter = 'blur(2px)';
+})
+
+exit.addEventListener('click', () => {
+    nav.classList.remove('open-nav');
+    wrapper.style.filter = 'blur(0)';
+})
